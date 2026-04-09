08ee9dc7dcc2c76f58061841c24111eba9ccbddf
EduardoCastA <89036982+EduardoCastA@users.noreply.github.com>
2026-04-08 21:47:07 -0600

    Crear migración para editar tabla de usuarios y comentar sedder de usuarios

---
+++ database/migrations/2026_04_09_033709_alter_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'name',
+                'email',
+                'email_verified_at',
+                'password',
+            ]);
+
+            $table->string('toka_id')->nullable()->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['toka_id']);
+
+            $table->string('name')->nullable()->after('id');
+            $table->string('email')->unique()->nullable()->after('name');
+            $table->timestamp('email_verified_at')->nullable()->after('email');
+            $table->string('password')->nullable()->after('email_verified_at');
+        });
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             GameSeeder::class,
-            UserSeeder::class,
+            // UserSeeder::class,
         ]);
     }
 }
